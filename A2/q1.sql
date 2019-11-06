64c60e71cf2cdb518f68cbfb200ca9398a2aa4ca
izaakniksan <izaakniksan@gmail.com>
2019-11-06 16:01:31 -0500

    now only rides that are completed are considered

--- A2/q1.sql
+++ A2/q1.sql
@@ -13,15 +13,22 @@ create table q1(
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
+DROP VIEW IF EXISTS Rides CASCADE;
 DROP VIEW IF EXISTS UniqueMonths CASCADE;
 
 
--- Define views for your intermediate steps here:
-CREATE VIEW UniqueMonths AS
-SELECT DISTINCT cast((cast(extract(year from datetime) as varchar(7)) || cast(extract(month from datetime) as varchar(7))) as int) as month, client_id
-FROM Request;
+-- These are the rides that were completed. The datetimes used here are 
+-- those of the request, not the drop off
+CREATE VIEW Rides AS
+SELECT Dropoff.request_id request_id, Request.datetime datetime, client_id
+FROM Dropoff JOIN Request on Dropoff.request_id=Request.request_id;
 
--- Your query that answers the question goes below the "insert into" line:
+-- These are the unique months that a given client has had ride(s) in
+CREATE VIEW UniqueMonths AS
+SELECT DISTINCT cast(extract(year from datetime) as varchar(7)) ||  
+cast(extract(month from datetime) as varchar(7)) as month, client_id
+FROM Rides ;
+
 insert into q1
 SELECT Client.client_id,email,count(month)
 FROM Client  JOIN UniqueMonths ON Client.client_id=UniqueMonths.client_id
